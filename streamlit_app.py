5ad52e25bdcb1d8c7be49b1d1880346109aec803
Eudaimonia Admin <84097653+eudaimoniatech@users.noreply.github.com>
2023-10-14 01:52:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import streamlit_pydantic as sp
 
+from models import Error
+from models import Brand
+from models import Brands
+from models import Searches
+
+models = [Brand]
+model = st.sidebar.radio(
+    label="Which Model to Use in Form",
+    options=models,
+    format_func=lambda x: x.__name__,
+)
+input_data = sp.pydantic_form(key="pydantic_form", model=model)
+
+if not input_data:
+    st.warning("Submit the Form to continue")
+    st.stop()
+
+st.code(repr(input_data))
+st.json(input_data.json())
